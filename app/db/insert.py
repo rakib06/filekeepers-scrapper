b9605fdf933dfe8966139e2d7415ff68f9dc5e26
Rakibul Hasan <rakibul.hsn47@gmail.com>
2022-08-20 00:19:31 +0600

    add mobile number

--- app/db/insert.py
+++ app/db/insert.py
@@ -1,11 +1,16 @@
 
 from db.conn import *
+import random
 
 myclient, mydb, mycol = connection()
 
 
 def insert_test():
-    mydict = [{"name": "John", "address": "Highway 37"}, ]
+    mobile = random.randint(1000000, 9000000)
+
+    mydict = [
+        {"name": "John", "address": "Highway 37", "mobile": mobile},
+    ]
     x = mycol.insert_many(mydict)
     with open('data/insert.txt', 'w+') as f:
         f.write(f"inserted_ids= {x.inserted_ids}")
